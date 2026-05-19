b144648298cf018ed535485a29a9c7a48365a31b
Robolicious11 <55985854+Robolicious11@users.noreply.github.com>
2026-05-19 13:50:30 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -808,7 +808,7 @@ LANDING_HTML = r'''<!DOCTYPE html>
       </div>
 
       <div id="ai-fields" style="display:none;">
-        <div class="ai-note">De AI moet nog verder geschreven worden, maar met paarden op steroïden is winnen al moeilijk zat.</div>
+        <div class="ai-note">Laat maar weten als de AI sterker moet</div>
         <div class="field">
           <label for="difficulty">AI DIFFICULTY</label>
           <select name="difficulty" id="difficulty">
